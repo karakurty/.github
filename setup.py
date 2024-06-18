60d1c90a7ec31d301d284009a06b043a9c9cf728
Valeriy Fezler <karakurty@gmail.com>
2024-06-18 11:55:04 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="hello_world_package",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A simple Hello World package",
+    author="Tim Warner",
+    author_email="tim@timw.info",
+    url="https://github.com/timothywarner/.github",
+)
